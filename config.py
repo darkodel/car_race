64f331f3cc5e08ab5d172465db5bc71b6f2af1d3
darkodel <53495116+darkodel@users.noreply.github.com>
2020-05-01 12:03:19 +0200

    Changed players.json to .players.json.

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ def load_config(config_file_path='config.json'):
     
     return(data)
 
-def load_score_history(score_history_path='players.json'):
+def load_score_history(score_history_path='.players.json'):
     with open(score_history_path) as score_history:
         data = json.load(score_history)
 
     return(data)
 
-def edit_score_history(data, score_history_path='players.json'):
+def edit_score_history(data, score_history_path='.players.json'):
     with open(score_history_path, 'w+') as score_history:
         json.dump(data, score_history)
 

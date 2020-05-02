0ea9653bece62ccd322e1722ed997630e32f2cf8
darkodel <53495116+darkodel@users.noreply.github.com>
2020-05-02 22:43:35 +0200

    Added config files' paths as "constants. Changed score history file name to .config_players.json.

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 import json
 
-def load_config(config_file_path='config.json'):
+CONFIG = '.config.json'
+CONFIG_PLAYERS = '.config_players.json'
+
+def load_config(config_file_path=CONFIG):
     with open(config_file_path) as config_file:
         data = json.load(config_file)
     
     return(data)
 
-def edit_score_history(data, score_history_path='.players.json'):
+def edit_score_history(data, score_history_path=CONFIG_PLAYERS):
     with open(score_history_path, 'w+') as score_history:
         json.dump(data, score_history)
 
@@ -20,10 +23,10 @@ def create_new_player(new_player):
     new_player_score_history = data['players']['player'] # get player's section
     new_player_score_history[0]['name'] = new_player # insert new player name
     data = load_score_history() # load the whole score history
-    data['player'].append(new_player_score_history[0]) # extend it with new player
+    data['player'].append(new_player_score_history[0]) # append new player to the player's list
     edit_score_history(data) # write it back to the file
 
-def load_score_history(score_history_path='.players.json'):
+def load_score_history(score_history_path=CONFIG_PLAYERS):
     try:
         with open(score_history_path) as score_history:
             data = json.load(score_history)
